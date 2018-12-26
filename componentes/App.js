dd84528bfa6711647bb8ac503c38160cf0d31f87
Daniel alvarez <montalvandaniel2@gmail.com>
2018-12-26 13:47:13 -0600

    listo

--- componentes/App.js
+++ componentes/App.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react';
 import Header from './header';
 import Formulario from './Formulario';
 import Resumen from './Resumen';
+import Resultado from './Resultado';
 import  {obtenerDiferenciaAnio} from './helper';
 import  {calcularMarca} from './helper';
 import  {obtenerPlan} from './helper';
-import  {primeraMayuscula} from './helper';
+
 class App extends Component {
 state={
   resultado:'',
@@ -35,11 +36,13 @@ datos:datosAuto
       <Header
        titulo="Cotizador de seguro de auto" />
             <div className='contenedor-formulario'>
+            
                 <Formulario  cotizarSeguro={this.cotizarSeguro}
                 />
+                
+<Resumen datos={this.state.datos}/>
+<Resultado resultado={this.state.resultado}/> 
             </div>
-<Resumen datos={this.state.datos}
-resultado={this.state.resultado}/>
 
     </div>
     );
--- componentes/Resumen.js
+++ componentes/Resumen.js
@@ -1,10 +1,26 @@
 import React,{Component} from 'react';
-
+import  {primeraMayuscula} from './helper';
 class Resumen extends Component{
-render(){
-    return(
-<p>Listo listo</p>
 
+    mostrarResumen=()=>{
+        const {marca,year,plan}= this.props.datos;
+        if (!marca || !year|| !plan)return <div className='resumen'> Elige marca ,año y tipo de seguro.</div>
+        return (
+<div className='resumen'>
+<h2>Resumen de Cotización </h2>
+<li>Marca:{primeraMayuscula(marca)}</li>
+<li>Plan:{primeraMayuscula(plan)}</li>
+<li>Año de Auto:{year}</li>
+</div>
+)
+    
+}
+    render(){
+    
+    return(
+<div>
+{this.mostrarResumen()}
+</div>
     )
 }
 

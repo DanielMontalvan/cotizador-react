943fd42d3453c7d5806ed3cba0ad60554e5012d9
Daniel alvarez <montalvandaniel2@gmail.com>
2018-12-26 15:33:33 -0600

    Proyecto finalizado

--- componentes/Resultado.js
+++ componentes/Resultado.js
@@ -4,14 +4,16 @@ import {TransitionGroup,CSSTransition} from 'react-transition-group';
 class Resultado extends Component{
 render(){
     const resultado= this.props.resultado;
+    const mensaje=(!resultado)?'Elige marca año o tipo de seguro.':resultado;
 return (
     <div className='gran-total'>
 <TransitionGroup component='span' className='resultado'>
 <CSSTransition classNames='resultado' key={resultado} timeout={{enter:500,exit:500}}>
+<span>{mensaje}</span>
 </CSSTransition>
 </TransitionGroup>
 </div>
-)  
+)
   }
  }
 export default Resultado;
